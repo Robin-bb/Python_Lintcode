01301045462597acfa618396e0bc2eaa755ca6c2
Robin-bb <66224356+Robin-bb@users.noreply.github.com>
2022-11-13 19:15:22 -0600

    Create [2274]First Python Code: Print Hello World.py

---
+++ [2274]First Python Code: Print Hello World.py
@@ -0,0 +1,8 @@
+# Description
+# This an entry level Python practice. In this problem, your task is to print Hello World to console (standard output) by using the method print in Python.
+
+# Example
+# This program without any input data. Your program should output Hello World to standard output.
+
+#Solution:
+print("Hello World")
