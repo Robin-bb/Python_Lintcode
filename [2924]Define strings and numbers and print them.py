6557c0857dee9c24b68f4a470687854dfcae6184
Robin-bb <66224356+Robin-bb@users.noreply.github.com>
2022-11-13 19:22:13 -0600

    Create [2924]Define strings and numbers and print them.py

---
+++ [2924]Define strings and numbers and print them.py
@@ -0,0 +1,23 @@
+# Description
+# In this topic, your task is to define a string 'Hello world' in terms of variables, and an integer number 1, and then use the print function in Python to set.
+
+# the type of the string variable you defined.
+# the type of the numeric variable.
+# the string variable.
+# the numeric variable.
+# Print to console output (standard output)
+# Example
+# This topic does not have input data. Instead, your code needs to print the result in the standard output as required by the topic.
+# Output.
+
+# <class 'str'>
+# <class 'int'>
+# Hello world
+# 1
+
+#Solution:
+# Please write your code here
+print(type("Hello world"))
+print(type(1))
+print("Hello world")
+print(1)
