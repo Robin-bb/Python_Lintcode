bdf2b0bd80b662c07c6d793716f6f602350a1d3d
Robin-bb <66224356+Robin-bb@users.noreply.github.com>
2022-11-13 19:20:48 -0600

    Create [2920]Get the input data and print it.py

---
+++ [2920]Get the input data and print it.py
@@ -0,0 +1,25 @@
+# Description
+# The evaluator will execute your code by executing python solution.py <{input_path}, where you need to accept a standard input data and then print it out
+
+# Example
+# Example 1
+# We give the input data as.
+
+# 'senior'
+# You need to print out.
+
+# 'senior'
+# Sample 2
+# We give the input as
+
+# 'testing'
+# You need to print out.
+
+# 'Testing'
+
+#Solution:
+
+# Please write your code here
+b = input()
+print(b)
+
